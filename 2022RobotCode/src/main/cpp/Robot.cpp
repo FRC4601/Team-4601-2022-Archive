a57c1c16e42ac6ec9bd89219529ad87d0412748c
FRC4601 <60019160+FRC4601@users.noreply.github.com>
2022-02-06 14:16:37 -0500

    Add new CTRE dependencies
    Included CTRE dependencies
    Added test code for Falcon 500 motors

--- 2022RobotCode/src/main/cpp/Robot.cpp
+++ 2022RobotCode/src/main/cpp/Robot.cpp
@@ -1,11 +1,22 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+//  Goals:
+
+
+
+
+
+
+
 
 #include <frc/Joystick.h>
 #include <frc/TimedRobot.h>
 #include <frc/drive/DifferentialDrive.h>
 #include <frc/motorcontrol/PWMSparkMax.h>
+
+// CTRE Docs - https://docs.ctre-phoenix.com/en/stable/ch05a_CppJava.html
+#include <ctre/Phoenix.h>
 
 #include <iostream>
 #include <memory>
@@ -18,6 +29,10 @@
 #include <networktables/NetworkTableEntry.h>
 #include <networktables/NetworkTableValue.h>
 #include <wpi/span.h>
+
+// Declare variables
+TalonSRX shooter1 = {0}; // number refers to device id. Can be found in Tuner
+TalonSRX shooter2 = {1};
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class.
@@ -36,11 +51,16 @@ class Robot : public frc::TimedRobot {
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
     m_rightMotor.SetInverted(true);
-  }
+
+    shooter1.Set(ControlMode::PercentOutput, 0);
+  };
 
   void TeleopPeriodic() override {
     // Drive with tank style
-    m_robotDrive.TankDrive(m_leftStick.GetY(), m_rightStick.GetY());
+    //m_robotDrive.TankDrive(m_leftStick.GetY(), m_rightStick.GetY());
+
+    shooter1.Set(ControlMode::PercentOutput, m_leftStick.GetY());
+    shooter2.Set(ControlMode::Follower, 5); // I'm unsure exactly how the 'Follower' control mode works. Needs testing
   }
 };
 
